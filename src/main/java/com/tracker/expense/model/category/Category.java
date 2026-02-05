e64f21ba90052a8f07ac61804aa1b67cf99df701
Mohammed Afsal <mohammedafsaltut@gmail.com>
2026-02-05 12:19:19 +0530

    feat: Category Entity model

---
+++ src/main/java/com/tracker/expense/model/category/Category.java
@@ -0,0 +1,32 @@
+package com.tracker.expense.model.category;
+
+import com.tracker.expense.model.auth.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categories")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String category;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @PrePersist
+    @PreUpdate
+    public void normalizeCategory() {
+        if (category != null) category = category.trim().toLowerCase();
+    }
+}
